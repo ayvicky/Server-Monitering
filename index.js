2aa74c3855b3fcedaf7fccb8b5432b3c38f3743a
ay_vicky <ayvicky1@gmail.com>
2020-08-14 10:13:42 -0700

    Parsing HTTP Methods

--- index.js
+++ index.js
@@ -16,11 +16,16 @@ var server = http.createServer(function (req, res) {
   var path = parseUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
+
   // Send the response
   res.end("Hello world\n");
 
   // Log the request path
-  console.log("Request recieved on path: " + trimmedPath);
+  console.log(
+    "Request recieved on path: " + trimmedPath + " with method: " + method
+  );
 });
 
 // Start the server, and have it listen on port 3000
