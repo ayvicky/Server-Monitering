4e47386f82e1a6d19f0b017ea21a00a87d689b9a
ay_vicky <ayvicky1@gmail.com>
2020-08-19 05:12:56 -0700

    Using the debugger -- cont, next, out, repl commands etc - node inspect index-debug

---
+++ index-debug.js
@@ -0,0 +1,62 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+//  Dependencies
+var server = require("./lib/server");
+var workers = require("./lib/workers");
+var cli = require('./lib/cli');
+var exampleDebuggingProblem = require('./lib/exampleDebuggingProblem');
+
+// Declare the app
+var app = {};
+
+// Init function
+app.init = function () {
+  // Start the server
+  server.init();
+
+  // Start the workers
+  debugger;
+  workers.init();
+  debugger;
+
+  // Start the CLI, but make sure it starts last
+  debugger;
+  setTimeout(function(){
+    cli.init();
+  }, 50);
+  debugger;
+
+  // Set foo at 1
+  debugger;
+  var foo = 1;
+  console.log('Just assigned 1 to foo');
+  debugger;
+  // Increatement foo
+  foo++;
+  console.log('Just incremented foo');
+  debugger;
+
+  // Square foo
+  foo = foo * foo;
+  console.log('Just squared foo');
+  debugger;
+
+  // Convert foo to a string
+  foo = foo.toString();
+  console.log('Just converted foo to string');
+  debugger;
+
+  // Call the init script that will throw
+  exampleDebuggingProblem.init();
+  console.log('Just called the library');
+  debugger;
+};
+
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
